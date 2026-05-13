ca5227feab33d7914d87aba73f60611549c7e4de
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-05-13 10:48:34 -0600

    Master parte 2

---
+++ IRepositorioPalabras.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado
+{
+    public interface IRepositorioPalabras
+    {
+        string ObtenerPalabraAleatoria();
+    }
+}
+
